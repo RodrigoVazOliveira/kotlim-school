a7c12429111872fbb57b5e896a705795b27c0b73
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-08-14 18:17:34 -0300

    Usando indices

---
+++ br/dev/rvz/arrays/Salary.kt
@@ -0,0 +1,12 @@
+package br.dev.rvz.arrays
+
+fun main() {
+    val salaries: DoubleArray = doubleArrayOf(500.50, 2300.00, 5000.0, 8000.00, 10000.0)
+
+    val increase = 1.1
+    for (i in salaries.indices) {
+        salaries[i] = salaries[i] * increase
+    }
+
+    println(salaries.contentToString())
+}
