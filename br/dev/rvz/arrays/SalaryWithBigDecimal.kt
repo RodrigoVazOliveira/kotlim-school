a2cf8b7e888fb5a6af6901e4a3e08bcdcea56742
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-08-16 05:48:12 -0300

    usando reduce

--- br/dev/rvz/arrays/SalaryWithBigDecimal.kt
+++ br/dev/rvz/arrays/SalaryWithBigDecimal.kt
@@ -9,20 +9,31 @@ fun main() {
     println(salaries.contentToString())
     val mostSalary = BigDecimal("1.1")
     val salariesWithMost: Array<BigDecimal> =
-        salaries.map { salary ->
-            val salaryBAse = BigDecimal("5000")
-            if (salary < salaryBAse) {
-                salary.plus(BigDecimal("500"))
-            } else {
-                (salary * mostSalary).setScale(2, RoundingMode.UP)
-            }
-        }.toTypedArray()
+        salaries.map { salary -> calculateMostRelative(salary, mostSalary) }.toTypedArray()
 
     println("Salarios com aumento: ${salariesWithMost.contentToString()}")
+    val sumSalaries = salariesWithMost.sum()
+    println("A soma do salario com aumento e de $sumSalaries")
+}
+
+private fun calculateMostRelative(
+    salary: BigDecimal,
+    mostSalary: BigDecimal
+): BigDecimal {
+    val salaryBAse = BigDecimal("5000")
+    return if (salary < salaryBAse) {
+        salary.plus(BigDecimal("500"))
+    } else {
+        (salary * mostSalary).setScale(2, RoundingMode.UP)
+    }
 }
 
 fun bigDecimalArrayOf(vararg values: String): Array<BigDecimal> {
     val size = values.size
 
     return Array(size) { index -> BigDecimal(values[index]) }
+}
+
+fun Array<BigDecimal>.sum(): BigDecimal {
+    return this.reduce { acumulator, value -> acumulator.plus(value) }
 }
