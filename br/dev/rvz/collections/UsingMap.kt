ba2ca4b3a0c6cd857e0cb4e662e4e28a86eae559
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-08-21 15:27:31 -0300

    Adicionar e remover dia acordo com valor

--- br/dev/rvz/collections/UsingMap.kt
+++ br/dev/rvz/collections/UsingMap.kt
@@ -14,10 +14,28 @@ fun main() {
         println("a chave da ordem e ${order.key}\nO valor da order e ${order.value}")
     }
 
-    orders.put(4, 2500.00)
+    orders[4] = 2500.00
+    orders[5] = 28000.00
 
     println("Novo map inserido: $orders")
 
     orders.remove(4)
     println("removido o numero 4: $orders")
+
+    println("Obter valor do cinco ${orders[5]}")
+
+
+    // adicionar se nao existir, se exitir manter o valor atual
+    orders.putIfAbsent(1, 8000.00)
+
+    println("validar se ouve modificacao no 1 ${orders[1]}")
+
+
+    // remover se o valor tambem e igual
+
+    orders.remove(1, 100.00)
+
+    // nao deve ser removido pelo fato do valor nao ser igual
+    println(orders)
+
 }
